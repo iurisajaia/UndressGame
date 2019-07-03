d7846a1c88072abb24f279bf09d1aabee360498f
iurisajaia <sajaiaiur@gmail.com>
2019-07-04 00:14:46 +0400

    ranking I

--- src/components/ranking/person.jsx
+++ src/components/ranking/person.jsx
@@ -3,10 +3,11 @@ import MedalSvg from "./medal";
 class Person extends Component {
   state = {};
   render() {
+    console.log(this.props)
     return (
       <li className="ranking-list-item">
         <div className="ranking-box">
-          <div className="rank">1.</div>
+          <div className="rank">{this.props.i}</div>
           <div className="person">
             <span className="name">{this.props.name}</span>
             <span className="point">{this.props.score}</span>
